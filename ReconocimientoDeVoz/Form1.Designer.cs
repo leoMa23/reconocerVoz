3c356a89aabfde9c853be6dd59f6940316d4d8ef
Leo <leonelfranco09@gmail.com>
2024-02-05 01:24:31 -0300

    subo a git la app de reconocimiento de voz

---
+++ ReconocimientoDeVoz/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace ReconocimientoDeVoz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDetener = new System.Windows.Forms.Button();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnDetener
+            // 
+            this.btnDetener.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnDetener.Location = new System.Drawing.Point(298, 88);
+            this.btnDetener.Name = "btnDetener";
+            this.btnDetener.Size = new System.Drawing.Size(75, 23);
+            this.btnDetener.TabIndex = 0;
+            this.btnDetener.Text = "Detener";
+            this.btnDetener.UseVisualStyleBackColor = false;
+            this.btnDetener.Click += new System.EventHandler(this.btnDetener_Click);
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnIniciar.Location = new System.Drawing.Point(395, 88);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(75, 23);
+            this.btnIniciar.TabIndex = 1;
+            this.btnIniciar.Text = "Iniciar deteccion";
+            this.btnIniciar.UseVisualStyleBackColor = false;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(29, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(441, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DodgerBlue;
+            this.ClientSize = new System.Drawing.Size(486, 123);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.btnDetener);
+            this.Name = "Form1";
+            this.Text = "Reconocimiento de Voz";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDetener;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ ReconocimientoDeVoz/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+
+namespace ReconocimientoDeVoz
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine oEscucha = new SpeechRecognitionEngine();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            oEscucha.SetInputToDefaultAudioDevice();
+            oEscucha.LoadGrammar(new DictationGrammar());
+            oEscucha.SpeechRecognized += Detection;
+            oEscucha.RecognizeAsync(RecognizeMode.Multiple);
+
+        }
+
+        private void Detection(object sender, SpeechRecognizedEventArgs e)
+        {
+            textBox1.Text = e.Result.Text;
+        }
+
+        private void btnDetener_Click(object sender, EventArgs e)
+        {
+            oEscucha.RecognizeAsyncStop();
+        }
+    }
+}
